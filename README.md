747b4b3991ca8f23835d217a96ef01d3f25affc9
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2018-07-31 10:48:33 +0800

    -

---
+++ README.md
@@ -0,0 +1,55 @@
+# jsnlib-joomla_database_eloquent
+輔助 Eloquent 在 Joomla! Model 中的使用
+
+##安裝
+````
+composer require jsnlib/joomla_database_eloquent
+````
+
+##使用方式
+
+使用別名為 Helper
+````php
+use Jsnlib\Joomla\Database\Eloquent\Helper as Helper;
+````
+
+請務必從外部連接資料庫以後，才指定給 Helper
+````php
+Helper::setEloquentName('Illuminate\Database\Capsule\Manager');
+
+Helper::proccess(true, function () use ($param)
+{
+
+});
+````
+
+##範例
+這裡示範一個連接的方式
+````php
+require_once 'vendor/autoload.php';
+use Jsnlib\Joomla\Database\Eloquent\Helper as Helper;
+use Illuminate\Database\Capsule\Manager as DB;
+
+$db = new DB;
+ 
+$db->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'test',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+ 
+$db->setAsGlobal();
+$db->bootEloquent();
+
+Helper::setEloquentName('Illuminate\Database\Capsule\Manager');
+
+Helper::proccess(true, function () use ($param)
+{
+    //....
+});
+````
--- src/Joomla/Database/Eloquent/Helper.php
+++ src/Joomla/Database/Eloquent/Helper.php
@@ -1,25 +1,25 @@
-<?php 
+<?php
 namespace Jsnlib\Joomla\Database\Eloquent;
 
 /**
- * 輔助 Eloquent 在 Model 中的使用 
+ * 輔助 Eloquent 在 Model 中的使用
  */
 class Helper
 {
     /**
      * 因為 Eloquent 在 Debug 需要較多參數設置，所以]包圍運行 DB 的外層，
      * 可控制是否啟用 Debug
-     *     
-     * @param  boolean  $isDebug  *false|true 是否啟用 debug
-     * @param  callable $callback 運行的主程序
-     * @return \Jsnlib\Ao         取得 debug 結果或是運行 $callback() 的數據
+     *
+     * @param  boolean    $isDebug  *false|true 是否啟用 debug
+     * @param  callable   $callback 運行的主程序
+     * @return \Jsnlib\Ao 取得 debug 結果或是運行 $callback() 的數據
      */
-    public  function proccess($isDebug = false, $callback)
+    public static function proccess($isDebug = false, $callback)
     {
         if ($isDebug === true)
         {
             \DB::connection()->enableQueryLog();
-            $result = $callback();
+            $result  = $callback();
             $queries = \DB::getQueryLog();
             print_r($queries);
         }
@@ -39,23 +39,26 @@ class Helper
 
     /**
      * 自動判斷查詢的複數或單數
-     * @param  \Jsnlib\Ao $result    由 self::proccess() 取得的 result
-     * @param  string     $info_list list 取得複數 | info 取得單數
-     * @return \Jsnlib\Ao            返回查詢結果 
+     * @param  \Jsnlib\Ao $result              由 self::proccess() 取得的 result
+     * @param  string     $info_list           list 取得複數 | info 取得單數
+     * @return \Jsnlib\Ao 返回查詢結果
      */
-    public  function selectResult($result, $info_list)
+    public static function selectResult($result, $info_list)
     {
         if ($info_list == "info")
         {
-            if (count($result) == 0) return $result;
-            
+            if (count($result) == 0)
+            {
+                return $result;
+            }
+
             return $result[0];
         }
         else if ($info_list == "list")
         {
             return $result;
         }
-        else 
+        else
         {
             throw new \Exception('需要指定 list | info');
         }
